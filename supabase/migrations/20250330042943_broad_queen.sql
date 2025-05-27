cb50d151d449eaca5b8bbde8c350a8a95d32346c
crisferre80 <cristianferreyra8076@gmail.com>
2025-05-27 19:12:39 +0000

    fix migrations clean

--- supabase/migrations/20250330042943_broad_queen.sql
+++
@@ -1,7 +0,0 @@
--- Add pickup_time to collection_claims
-ALTER TABLE collection_claims
-ADD COLUMN IF NOT EXISTS pickup_time timestamptz;
-
--- Add pickup_time to collection_points
-ALTER TABLE collection_points
-ADD COLUMN IF NOT EXISTS pickup_time timestamptz;
